c31bfa795d291b912b4aaa6b7bc99033abd64d08
Jithin <jithinbhaskar11@gmail.com>
2024-03-25 15:55:06 +0530

    login ui completed

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -3,12 +3,23 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_textfield.dart';
+import 'package:food_delivery_app/view/home_page.dart';
+import 'package:food_delivery_app/view/register_page.dart';
 
 class LoginPage extends StatelessWidget {
 
 
-  final emaillController=TextEditingController();
+  final emailController=TextEditingController();
   final passController=TextEditingController();
+
+
+  void login(BuildContext context){
+
+    ///validation
+    ///
+
+    Navigator.push(context, (MaterialPageRoute(builder: (context)=>HomePage())));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,18 +44,22 @@ class LoginPage extends StatelessWidget {
                     fontSize: 15,
                   )),
               SizedBox(height: 25,),
-              MyTextField(controller: emaillController, hintText: 'Enter your Email', obscureText: false),
+              MyTextField(controller: emailController, hintText: 'Enter your Email', obscureText: false),
               SizedBox(height: 10,),
               MyTextField(controller: passController, hintText: 'Enter Your Password', obscureText: true),
               SizedBox(height: 10,),
-              MyButton(text: 'LogIn', onTap: (){}),
+              MyButton(text: 'LogIn', onTap: (){
+                login(context);
+              }),
               SizedBox(height: 10,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text('New here?',style: TextStyle(color: Theme.of(context).colorScheme.primary),),
                   SizedBox(width: 4,),
-                  TextButton(onPressed: (){}, child:Text('Register here'))
+                  TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>RegisterPage()));
+                  }, child:Text('Register here'))
                 ],
               )
             ],
---
+++ lib/view/register_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/view/login_page.dart';
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+
+  final emailController=TextEditingController();
+  final passController=TextEditingController();
+  final cPassController=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.lock_open,
+                size: 100,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text('Food Delivery App',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  )),
+              SizedBox(height: 25,),
+              MyTextField(controller: emailController, hintText: 'Enter your Email', obscureText: false),
+              SizedBox(height: 10,),
+              MyTextField(controller: passController, hintText: 'Enter Your Password', obscureText: true),
+              SizedBox(height: 10,),
+              MyTextField(controller: cPassController, hintText: 'Re-enter Your Password', obscureText: true),
+              SizedBox(height: 10,),
+              MyButton(text: 'Sign Up', onTap: (){}),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already a user?',style: TextStyle(color: Theme.of(context).colorScheme.primary),),
+                  SizedBox(width: 4,),
+                  TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                  }, child:Text('LogIn  here'))
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
