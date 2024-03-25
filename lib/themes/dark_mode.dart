fc516d608f66b3c5933578a0f9c05ad74075227c
Jithin <jithinbhaskar11@gmail.com>
2024-03-25 12:30:57 +0530

    dark,light mode added

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode=ThemeData(
+    colorScheme: ColorScheme.light(
+        background: Colors.black26,
+        primary: Colors.black45,
+        secondary: Colors.black12,
+        tertiary: Colors.black,
+        inversePrimary: Colors.black87
+    )
+);
---
+++ lib/view/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.lock_open,
+              size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary ,
+            ),
+            SizedBox(height: 25,),
+            Text('Food Delivery App',
+            style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary,fontWeight: FontWeight.bold,fontSize: 15),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
